26e6edfa06d24114e152d152584b052a61ec0b5e
Rominoushka <rom1.landrieau@gmail.com>
2026-06-16 14:50:35 +0200

    Force NOEUD pour les equipements specifiques

--- test_export_part1.js
+++ test_export_part1.js
@@ -1,1 +1,7 @@
-function expJSON(){deliver('land2map_validation_v23.json',JSON.stringify({version:'v23',date:new Date().toISOString(),objets:O.map(patch)},null,2))}
+var _ffiles_noeud=ffiles;
+ffiles=function(o){
+  var r=_ffiles_noeud(o);
+  if(o && String(o.code||'').endsWith('_EQUIPEMENT_SPECIFIQUE')) r.bloc_dwg='NOEUD';
+  return r;
+};
+function expJSON(){deliver('land2map_validation_v23.json',JSON.stringify({version:'v37',date:new Date().toISOString(),objets:O.map(patch)},null,2))}
