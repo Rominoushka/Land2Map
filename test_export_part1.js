268190ba221b272306e9d5af1b776190e1a22e54
Rominoushka <rom1.landrieau@gmail.com>
2026-06-16 16:05:04 +0200

    Ajoute materiau AEP et equipements AEP

--- test_export_part1.js
+++ test_export_part1.js
@@ -1,26 +1,39 @@
 (function(){
-  O=O.filter(function(o){return o.code!='BTA_APPUI'&&o.code!='HTA_APPUI'&&!(o.n=='ECL'&&o.t=='fourreau')});
+  O=O.filter(function(o){return o.code!='BTA_APPUI'&&o.code!='HTA_APPUI'&&!(o.n=='ECL'&&o.t=='fourreau')&&!(o.n=='TEL'&&o.t=='fourreau')});
   if(C.lin&&C.lin.ECL)C.lin.ECL=C.lin.ECL.filter(function(x){return String(x).toUpperCase()!='FOURREAU'});
+  if(C.lin&&C.lin.TEL)C.lin.TEL=C.lin.TEL.filter(function(x){return String(x).toUpperCase()!='FOURREAU'});
+  if(C.pts&&C.pts.AEP){
+    var aepPts=C.pts.AEP.split(/\s+/).filter(Boolean);
+    ['COMPTEUR','ARMOIRE'].forEach(function(x){if(aepPts.indexOf(x)<0)aepPts.push(x)});
+    C.pts.AEP=aepPts.join(' ');
+  }
+  function exists(code){return O.some(function(o){return o.code==code})}
+  if(!exists('AEP_COMPTEUR'))add('AEP','AEP_COMPTEUR','Equipements','equipement','compteur');
+  if(!exists('AEP_ARMOIRE'))add('AEP','AEP_ARMOIRE','Equipements','equipement','armoire');
   var vals=C.vals||{};
   Object.keys(vals).forEach(function(k){var nk=na(k);if(nk!==k){vals[nk]=vals[k];delete vals[k];}});
   vals.lst_sup_materiau=['Acier','Aluminium','Bois','Béton','Composite','Fonte','Inconnu'];
   vals.lst_type_point_lumineux=['LED','Halogène','Sodium','Iodure métallique','Fluorescent','Incandescent','Inconnu'];
+  vals.lst_can_mat=['pvc','pvca','pehd','acier','be','inc'];
   C.vals=vals;
   function has(o,c){return o.attrs.some(function(a){return a.c==na(c)})}
-  function add(o,a){if(!has(o,a.c))o.attrs.push(a)}
+  function addAttr(o,a){if(!has(o,a.c))o.attrs.push(a)}
   O.forEach(function(o){
     o.attrs=o.attrs.map(function(a){a.c=na(a.c);a.li=a.li?na(a.li):'';return a});
     if(o.f=='support'){
-      add(o,A('sup_haut','Nombre','Hauteur support','','',true,false,false,true));
-      add(o,A('sup_mat','Liste','Matériau support','lst_sup_materiau','',true,false,false,true));
+      addAttr(o,A('sup_haut','Nombre','Hauteur support','','',true,false,false,true));
+      addAttr(o,A('sup_mat','Liste','Matériau support','lst_sup_materiau','',true,false,false,true));
     }
     if(o.t=='candelabre_multiple'){
-      add(o,A('nb_pl','Nombre','Nombre de points lumineux','','',true,false,false,true));
-      add(o,A('typ_pl','Liste','Type de point lumineux','lst_type_point_lumineux','',true,false,false,true));
+      addAttr(o,A('nb_pl','Nombre','Nombre de points lumineux','','',true,false,false,true));
+      addAttr(o,A('typ_pl','Liste','Type de point lumineux','lst_type_point_lumineux','',true,false,false,true));
+    }
+    if(o.n=='AEP'&&o.t=='canalisation'){
+      addAttr(o,A('trc_mat','Liste','Matériau canalisation','lst_can_mat','',true,false,true,true));
     }
   });
 })();
-var extraBlocks1={AEP_CHAMBRE:'REGARD_RECTANGULAIRE',DEI_BOUCHE_INCENDIE_BI:'REGARD_RECTANGULAIRE',EUS_BOITE_BRANCHEMENT:'REGARD_RECTANGULAIRE',EUS_POSTE_RELEVAGE:'REGARD_CIRCULAIRE',EPL_BOITE_BRANCHEMENT:'REGARD_RECTANGULAIRE',EPL_POSTE_RELEVAGE:'REGARD_CIRCULAIRE',EPL_EXUTOIRE:'NOEUD',EPL_BASSIN_RETENTION:'NOEUD',EPL_PUISARD:'PUITS',EPL_TETE_BUSE:'AQUEDUC',EPL_AVALOIR:'AVALOIR_TAMPON',EPL_GRILLE_AVALOIR:'AVALOIR_GRILLE'};
+var extraBlocks1={AEP_CHAMBRE:'REGARD_RECTANGULAIRE',AEP_COMPTEUR:'COMPTEUR',AEP_ARMOIRE:'ARMOIRE',DEI_BOUCHE_INCENDIE_BI:'REGARD_RECTANGULAIRE',EUS_BOITE_BRANCHEMENT:'REGARD_RECTANGULAIRE',EUS_POSTE_RELEVAGE:'REGARD_CIRCULAIRE',EPL_BOITE_BRANCHEMENT:'REGARD_RECTANGULAIRE',EPL_POSTE_RELEVAGE:'REGARD_CIRCULAIRE',EPL_EXUTOIRE:'NOEUD',EPL_BASSIN_RETENTION:'NOEUD',EPL_PUISARD:'PUITS',EPL_TETE_BUSE:'AQUEDUC',EPL_AVALOIR:'AVALOIR_TAMPON',EPL_GRILLE_AVALOIR:'AVALOIR_GRILLE'};
 var _ffiles_noeud=ffiles;
 ffiles=function(o){
   var r=_ffiles_noeud(o);
@@ -28,4 +41,4 @@ ffiles=function(o){
   if(o && String(o.code||'').endsWith('_EQUIPEMENT_SPECIFIQUE')) r.bloc_dwg='NOEUD';
   return r;
 };
-function expJSON(){deliver('land2map_validation_v37.json',JSON.stringify({version:'v37',date:new Date().toISOString(),objets:O.map(patch)},null,2))}
+function expJSON(){deliver('land2map_validation_v39.json',JSON.stringify({version:'v39',date:new Date().toISOString(),objets:O.map(patch)},null,2))}
